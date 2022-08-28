c758b8fc6c5e3110647c9069be69448cd4e230b3
Ben Eisner <ben.a.eisner@gmail.com>
2022-08-28 18:26:16 +0100

    add a render dataset class, to iterate over all things

--- partnet_mobility_utils/dataset.py
+++ partnet_mobility_utils/dataset.py
@@ -1,6 +1,11 @@
+import copy
+import logging
 import typing
 from pathlib import Path
-from typing import Iterable, List, Tuple, Union
+from typing import Dict, Iterable, List, Literal, Tuple, Union
+
+from partnet_mobility_utils.data import PMObject
+from partnet_mobility_utils.render.pybullet import PybulletRenderer
 
 
 def write_ids(path: Union[str, Path], objs: Iterable[Tuple[str, str]]):
@@ -23,7 +28,86 @@ WELL_FORMED = {obj[0] for obj in read_ids(__parent / "data_lists" / "well_formed
 SUFFICIENT = {obj[0] for obj in read_ids(__parent / "data_lists" / "extra_objs.txt")}
 DEFAULT_OBJS = list(WELL_FORMED.union(SUFFICIENT))
 
+UMPNET_DIR = Path(__file__).parent / "data_lists" / "umpnet"
+
 
 def get_ids_by_class(class_name: str, filter_valid: bool = True) -> List[str]:
     ids = read_ids(Path(__file__).parent / "data_lists" / f"{class_name}.txt")
     return [id[0] for id in ids if ((id in DEFAULT_OBJS) or not filter_valid)]
+
+
+AVAILABLE_DATASET = Literal[
+    "all", "umpnet-train-train", "umpnet-train-test", "umpnet-test"
+]
+
+
+class PCDataset:
+    """Partnet-Mobility Point Cloud Dataset"""
+
+    def __init__(
+        self,
+        root: Union[str, Path],
+        split: Union[AVAILABLE_DATASET, List[str]],
+        randomize_joints: bool = False,
+        randomize_camera: bool = False,
+        renderer: Literal["pybullet", "sapien", "trimesh"] = "pybullet",
+    ):
+        if isinstance(split, str):
+            if split == "all":
+                self._ids = DEFAULT_OBJS
+            else:
+                ids = read_ids(
+                    {
+                        "umpnet-train-train": UMPNET_DIR / "train_train.txt",
+                        "umpnet-train-test": UMPNET_DIR / "train_test.txt",
+                        "umpnet-test": UMPNET_DIR / "test.txt",
+                    }[split]
+                )
+                self._ids = [id[0] for id in ids]
+        else:
+            self._ids = copy.deepcopy(split)
+
+        new_ids = []
+        def_objs = set(DEFAULT_OBJS)
+        for id in self._ids:
+            if id not in def_objs:
+                logging.warning(f"{id} is not well-formed, excluding...")
+                raise ValueError("BDADAD")
+            else:
+                new_ids.append(id)
+
+        self._ids = new_ids
+
+        self.pm_objs: Dict[str, PMObject] = {
+            id: PMObject(Path(root) / id) for id in self._ids
+        }
+        self.renderers: Dict[str, PybulletRenderer] = {}
+        self.renderer_type = renderer
+        self.randomize_joints = randomize_joints
+        self.randomize_camera = randomize_camera
+
+    def __getitem__(self, item: Union[int, str]):
+        if isinstance(item, str):
+            obj_id = item
+        else:
+            obj_id = self._ids[item]
+
+        obj = self.pm_objs[obj_id]
+
+        if obj_id not in self.renderers:
+            if self.renderer_type == "pybullet":
+                renderer = PybulletRenderer()
+            else:
+                raise NotImplementedError("not yet implemented")
+            self.renderers[obj_id] = renderer
+
+        renderer = self.renderers[obj_id]
+
+        pc_info = renderer.render(
+            obj, "all" if self.randomize_joints else False, self.randomize_camera
+        )
+
+        return pc_info
+
+    def __len__(self):
+        return len(self._ids)
---
+++ tests/pm_dataset_test.py
@@ -0,0 +1,37 @@
+import os.path
+import random
+from pathlib import Path
+
+import pytest
+
+from partnet_mobility_utils.dataset import PCDataset
+
+PM_DATASET_EXISTS = os.path.exists(
+    os.path.expanduser("~/datasets/partnet-mobility/raw")
+)
+
+
+@pytest.mark.skipif(not PM_DATASET_EXISTS, reason="pm not found")
+def test_full_dataset_creation():
+    dset = PCDataset(
+        root=os.path.expanduser("~/datasets/partnet-mobility/raw"), split="all"
+    )
+    random.seed(0)
+    for i in range(10):
+        data = dset[random.randint(0, len(dset))]
+
+
+@pytest.mark.skipif(not PM_DATASET_EXISTS, reason="pm not found")
+def test_umpnet_creation():
+    dset = PCDataset(
+        root=os.path.expanduser("~/datasets/partnet-mobility/raw"),
+        split="umpnet-test",
+    )
+    random.seed(0)
+    for i in range(10):
+        data = dset[random.randint(0, len(dset) - 1)]
+
+
+def test_single_dataset():
+    dset = PCDataset(root=Path(__file__).parent / "testdata", split=["100809"])
+    pc = dset[0]
